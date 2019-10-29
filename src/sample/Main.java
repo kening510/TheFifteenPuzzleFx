69a9d12556280a3beaf07c889f3c95e8c02dc32f
Kening Fohlin <kening.fohlin@gmail.com>
2019-10-29 20:48:43 +0100

    added moveAction method and exitGame method

--- src/sample/Main.java
+++ src/sample/Main.java
@@ -1,14 +1,18 @@
 package sample;
+
 import javafx.application.Application;
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
 import javafx.geometry.Insets;
+import javafx.scene.Node;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.scene.image.Image;
 import javafx.scene.layout.GridPane;
 import javafx.scene.layout.HBox;
 import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
+
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
@@ -17,16 +21,24 @@ public class Main extends Application {
     GridPane puzzleLayout = new GridPane();
     VBox designLayout = new VBox();
     HBox buttonLayout = new HBox();
+    private Button button;
     private Button newGameButton;
     private Button exitButton;
-    private Button highScoreListButton;
+    private Button highScoreButton;
     private List<Button> buttonList = new ArrayList<>();
+    private int counter;
 
 
     public void createAllButtons() {
         int buttonNr = 0;
         for (int i = 0; i < 15; i++){
-            Button button = new Button("" + ++buttonNr);
+            button = new Button("" + ++buttonNr);
+            button.setOnAction(new EventHandler<ActionEvent>() {
+                @Override
+                public void handle(ActionEvent actionEvent) {
+                    moveAction((Button)actionEvent.getSource());
+                }
+            });
             button.setMinWidth(50);
             if(buttonNr%2 == 0){
                 button.setId("even");
@@ -42,13 +54,54 @@ public class Main extends Application {
         newGameButton.setId("menuButton");
         exitButton = new Button("Exit");
         exitButton.setId("menuButton");
-        highScoreListButton = new Button("High Score");
-        highScoreListButton.setId("menuButton");
+        highScoreButton = new Button("High Score");
+        highScoreButton.setId("menuButton");
         buttonLayout.getChildren().add(newGameButton);
         buttonLayout.getChildren().add(exitButton);
-        buttonLayout.getChildren().add(highScoreListButton);
+        buttonLayout.getChildren().add(highScoreButton);
         designLayout.getChildren().add(buttonLayout);
     }
+
+    public void moveAction(Button clickedButton){
+        int columnIndex = GridPane.getColumnIndex(clickedButton);
+        int rowIndex = GridPane.getRowIndex(clickedButton);
+
+        if(getButtonFromPuzzle(columnIndex-1,rowIndex) == null){
+            if(columnIndex-1>=0){
+            puzzleLayout.getChildren().remove(clickedButton);
+            puzzleLayout.add(clickedButton,columnIndex-1,rowIndex);
+            }
+        }
+        if(getButtonFromPuzzle(columnIndex+1,rowIndex) == null){
+            if(columnIndex+1<=3){
+            puzzleLayout.getChildren().remove(clickedButton);
+            puzzleLayout.add(clickedButton,columnIndex+1,rowIndex);
+            }
+        }
+
+        if(getButtonFromPuzzle(columnIndex,rowIndex-1) == null){
+            if(rowIndex-1 >= 0){
+            puzzleLayout.getChildren().remove(clickedButton);
+            puzzleLayout.add(clickedButton,columnIndex,rowIndex-1);
+            }
+        }
+        if (getButtonFromPuzzle(columnIndex,rowIndex+1) == null){
+            if(rowIndex+1 <= 3){
+            puzzleLayout.getChildren().remove(clickedButton);
+            puzzleLayout.add(clickedButton,columnIndex,rowIndex+1);
+            }
+        }
+    }
+
+    public Node getButtonFromPuzzle(int col, int row){
+        for(Node node: puzzleLayout.getChildren()){
+            if(GridPane.getColumnIndex(node) == col && GridPane.getRowIndex(node) == row){
+                return node;
+            }
+        }
+        return null;
+    }
+
 
     public void addNewGameAction() {
         newGameButton.setOnAction(new EventHandler<ActionEvent>() {
@@ -59,10 +112,28 @@ public class Main extends Application {
         });
     }
 
+    public void exitGame(){
+        exitButton.setOnAction(new EventHandler<ActionEvent>() {
+            @Override
+            public void handle(ActionEvent actionEvent) {
+                System.exit(0);
+            }
+        });
+    }
+
+    public void getHighScoreList(){
+        highScoreButton.setOnAction(new EventHandler<ActionEvent>() {
+            @Override
+            public void handle(ActionEvent actionEvent) {
+
+            }
+        });
+    }
+
+
     public void shuffleButton() {
         Collections.shuffle(buttonList);
         emptyPuzzleLayout();
-        int counter = 0;
         for (int y = 0; y <= 3; y++) {
             for (int x = 0; x <= 3; x++) {
                 if(counter == 15){
@@ -91,16 +162,14 @@ public class Main extends Application {
         buttonLayout.setPadding(new Insets(10));
         Scene scene = new Scene(designLayout, 350, 305);
         primaryStage.setScene(scene);
-
+        primaryStage.getIcons().add(new Image("File:pikachu.png"));
         scene.getStylesheets().add(Main.class.getResource("Style.css").toExternalForm());
         createAllButtons();
         shuffleButton();
         addNewGameAction();
+        exitGame();
         primaryStage.show();
-
-
     }
-
 
     public static void main(String[] args) {
         launch(args);
